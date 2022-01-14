5967da00c8a97e6349536dab583a3bf316e6a6fe
Maxim <markushitstep@gmail.com>
2022-01-14 17:19:34 +0200

    meta tags

--- pages/posts.js
+++ pages/posts.js
@@ -1,5 +1,10 @@
+import Head from "next/head";
+
 export default function posts() {
     return <>
+        <Head>
+            <title>Posts Page</title>
+        </Head>
         <h1>Posts Page</h1>
         <p>lorem ipsum dolor sit amet</p>
     </>
