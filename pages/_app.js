b03f93ec00173bd561cdddde4eb4969e7ce6c326
Maxim <markushitstep@gmail.com>
2022-01-19 15:18:57 +0200

    install loading indicator

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,17 @@
 import './../styles/_app.css'
+import NextNprogress from 'nextjs-progressbar';
 
 export default function MyApp({ Component, pageProps }) {
     return (
         <>
+            <NextNprogress
+                color="lightgrey"
+                startPosition={0.3}
+                stopDelayMs={200}
+                height={3}
+                showOnShallow={true}
+            />
             <Component {...pageProps} />
-            
         </>
     )
 }
--- pages/about/index.js
+++ pages/about/index.js
@@ -1,7 +1,7 @@
 import Router from 'next/router'
 import MainLayout from "../../components/MainLayout"
 
-export default function about() {
+export default function About({ title }) {
 
     const linkClickHandler = () => {
         Router.push('/posts');
@@ -9,9 +9,16 @@ export default function about() {
 
     return (
         <MainLayout title={"About page"}>
-            <h1>About</h1>
+            <h1>{ title }</h1>
             <button onClick={linkClickHandler}>Go to posts </button>
             <button onClick={() => Router.push('/')}>Go back to home</button>
         </MainLayout>
     )
+}
+
+About.getInitialProps = async ({}) =>{
+    const respose = await fetch("http://localhost:4200/about");
+    const data = await respose.json();
+
+    return { title: data.title }
 }
